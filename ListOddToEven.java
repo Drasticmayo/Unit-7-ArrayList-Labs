f1776e2d50a1da2c33613418358d44d97b428227
Jude Armstrong <134647784+Drasticmayo@users.noreply.github.com>
2025-02-05 09:40:23 -0500

    Update ListOddToEven.java

--- ListOddToEven.java
+++ ListOddToEven.java
@@ -9,6 +9,25 @@ public class ListOddToEven
 {
 	public static int go( ArrayList<Integer> ray )
 	{
-		return false;
-	}
-}
+        int firstOdd;
+        int lastEven;
+        for (int i; i < ray.size(); i++)
+        {
+            if (ray.get(i)%2 != 0)
+            {
+                firstOdd = ray.get(i);
+                break;
+            }
+        }
+        
+        for (int i = ray.size()-1; i >= 0; i--)
+        {
+            if (ray.get(i)%2 == 0)
+            {
+                lastEven = ray.get(i);
+                break;
+            }
+        }
+        int distance = ray.indexOf(firstOdd) - ray.indexOf(lastEven);
+        return distance;
+		return -1;
